473ad6ae569b525baeb5147a5f1895379cab1735
pman <pman-$-432894328923@gmail.com>
2024-03-19 09:41:14 +0100

    ...

--- scripts/common.sh
+++ scripts/common.sh
@@ -7,18 +7,18 @@ set -euxo pipefail
 # Variable Declaration
 
 # DNS Setting
-if [ ! -d /etc/systemd/resolved.conf.d ]; then
-	sudo mkdir /etc/systemd/resolved.conf.d/
-fi
-cat <<EOF | sudo tee /etc/systemd/resolved.conf.d/dns_servers.conf
-[Resolve]
+# if [ ! -d /etc/systemd/resolved.conf.d ]; then
+# 	sudo mkdir /etc/systemd/resolved.conf.d/
+# fi
+# cat <<EOF | sudo tee /etc/systemd/resolved.conf.d/dns_servers.conf
+# [Resolve]
+#
+# EOF
 
-EOF
-
-sudo systemctl restart systemd-resolved
+# sudo systemctl restart systemd-resolved
 
 # disable swap
-sudo swapoff -a
+# sudo swapoff -a
 
 # keeps the swaf off during reboot
 (
@@ -28,23 +28,23 @@ sudo swapoff -a
 sudo apt-get update -y
 
 # Create the .conf file to load the modules at bootup
-cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
-overlay
-br_netfilter
-EOF
-
-sudo modprobe overlay
-sudo modprobe br_netfilter
-
-# sysctl params required by setup, params persist across reboots
-cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-iptables  = 1
-net.bridge.bridge-nf-call-ip6tables = 1
-net.ipv4.ip_forward                 = 1
-EOF
-
-# Apply sysctl params without reboot
-sudo sysctl --system
+# cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+# overlay
+# br_netfilter
+# EOF
+#
+# sudo modprobe overlay
+# sudo modprobe br_netfilter
+#
+# # sysctl params required by setup, params persist across reboots
+# cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+# net.bridge.bridge-nf-call-iptables  = 1
+# net.bridge.bridge-nf-call-ip6tables = 1
+# net.ipv4.ip_forward                 = 1
+# EOF
+#
+# # Apply sysctl params without reboot
+# sudo sysctl --system
 
 ## Install CRIO Runtime
 
@@ -66,13 +66,14 @@ sudo systemctl start crio.service
 echo "CRI runtime installed susccessfully"
 
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt-get update -y
-sudo apt-get install -y kubelet="$KUBERNETES_VERSION" kubectl="$KUBERNETES_VERSION" kubeadm="$KUBERNETES_VERSION"
-sudo apt-get update -y
-sudo apt-get install -y jq
+# curl -fsSL https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v$KUBERNETES_VERSION_SHORT/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+#
+# sudo apt-get update -y
+# sudo apt-get install -y kubelet="$KUBERNETES_VERSION" kubectl="$KUBERNETES_VERSION" kubeadm="$KUBERNETES_VERSION"
+# sudo apt-get update -y
+# sudo apt-get install -y jq
 
 # local_ip="$(ip --json a s | jq -r '.[] | if .ifname == "eth1" then .addr_info[] | if .family == "inet" then .local else empty end else empty end')"
 # cat >/etc/default/kubelet <<EOF
